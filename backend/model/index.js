bb678ae0eadb6d059b73f44da7a2358b75af03d7
LandrieJ <landrie.jeanson@gmail.com>
2024-11-25 10:33:41 +0100

    test

--- backend/model/index.js
+++ backend/model/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("portail", "root", "", {
+const sequelize = new Sequelize("portail", "root", "root", {
     host: "localhost",
     dialect: 'mysql',
     
